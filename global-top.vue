b20e453188eee0b573a97368c8b6092517bc7790
Clydinite <clydinite@gmail.com>
2026-05-28 14:06:53 +0800

    added slide number

---
+++ global-top.vue
@@ -0,0 +1,6 @@
+<template>
+  <div v-if="$nav.currentLayout !== 'aura-hero'"
+    class="absolute bottom-4 right-5 flex items-center gap-4 opacity-30 font-mono text-[10px] tracking-widest uppercase text-white">
+    <span>{{ $slidev.nav.currentPage }}</span>
+  </div>
+</template>
